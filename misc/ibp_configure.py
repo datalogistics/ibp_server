ba2b26e98ddf4acb0f322d59211b137b87f19a5f
Akshay Dorwat <adorwat@indiana.edu>
2014-05-29 08:54:34 +0000

    Fixed bug in pulling list of all interfaces. Now Working both with 64 and 32 bit.

--- misc/ibp_configure.py
+++ misc/ibp_configure.py
@@ -153,6 +153,31 @@ def get_ip_address(ifname):
       )[20:24])
 
 def all_interfaces():
+    is_64bits = sys.maxsize > 2**32
+    struct_size = 40 if is_64bits else 32
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    max_possible = 8 # initial value                                                                                                                                                                         
+    while True:
+        bytes = max_possible * struct_size
+        names = array.array('B', '\0' * bytes)
+        outbytes = struct.unpack('iL', fcntl.ioctl(
+            s.fileno(),
+            0x8912,  # SIOCGIFCONF                                                                                                                                                                           
+            struct.pack('iL', bytes, names.buffer_info()[0])
+        ))[0]
+        if outbytes == bytes:
+            max_possible *= 2
+        else:
+            break
+    namestr = names.tostring()
+    lst = []
+    for i in range(0, outbytes, struct_size):
+        name = namestr[i:i+16].split('\0', 1)[0]
+        ip   = namestr[i+20:i+24]
+        lst.append((name, ip))
+    return lst
+
+def all_interfaces():
     max_possible = 128  # arbitrary. raise if needed.
     bytes = max_possible * 32
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
