3ad458e933c0ba0b334310f0ab64ee4399cbb765
Ezra Kissel <ezkissel@indiana.edu>
2015-09-15 20:15:46 -0400

    Updating leveldb check so it works with CentOS 7 EPEL package.

--- cmake/FindLevelDB.cmake
+++ cmake/FindLevelDB.cmake
@@ -8,29 +8,23 @@
 # LEVELDB_FOUND, If false, do not try to use LevelDB.
 # also defined, but not for general use are
 # LEVELDB_LIBRARY, where to find the LevelDB library.
+
+find_path(LEVELDB_INCLUDE_DIR
+    NAMES leveldb/c.h
+    HINTS ${LEVELDB_ROOT_DIR}/include)
  
-find_path(LEVELDB_INCLUDE_DIR leveldb/c.h PATHS
-    leveldb/include
-    NO_DEFAULT_PATH
-    )
- 
-set(LEVELDB_NAME leveldb)
-find_library(LEVELDB_LIBRARY NAMES libleveldb.a PATHS
-    leveldb/
-    NO_DEFAULT_PATH
-    )
- 
-if (LEVELDB_LIBRARY AND LEVELDB_INCLUDE_DIR)
-  set(LEVELDB_LIBRARIES ${LEVELDB_LIBRARY})
-  set(LEVELDB_FOUND "YES")
-  message(STATUS "Found LevelDB: ${LEVELDB_LIBRARY} ${LEVELDB_INCLUDE_DIR}")
-else (LEVELDB_LIBRARY AND LEVELDB_INCLUDE_DIR)
-  set(LEVELDB_FOUND "NO")
-  message(STATUS "Could not find LevelDB")
-endif (LEVELDB_LIBRARY AND LEVELDB_INCLUDE_DIR)
+find_library(LEVELDB_LIBRARIES
+    NAMES leveldb
+    HINTS ${LEVELDB_ROOT_DIR}/lib64)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(leveldb DEFAULT_MSG
+    LEVELDB_LIBRARIES
+    LEVELDB_INCLUDE_DIR)
  
 mark_as_advanced(
-  LEVELDB_LIBRARY
+  LEVELDB_ROOT_DIR
+  LEVELDB_LIBRARIES
   LEVELDB_INCLUDE_DIR
   )
  
